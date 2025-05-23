a1d7b8303fad1c32a8c9bd20f4ac77d1af8d47b5
Valdeclesia <smel73715@gmail.com>
2025-05-23 18:54:49 +0000

    Aula 23/05/2025 - props, children e rendernição usando map

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import { MyText } from "./components/MyText";
 export default function App() {
   return (
  <>
-  <MyText/>
-  <MyText/>
-  <MyText/>
+  <MyText title="meu titulo">
+  
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi,
+      rerum similique at cupiditate ratione sapiente sint vel error alias tenetur. 
+      Eius cupiditate consequatur saepe nisi quo repellendus error pariatur aut!
+
+
+  </MyText>
 </>
  
   );
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,21 +1,15 @@
 import styles from './MyText.module.css';
 
-export function MyText() {
+export function MyText({title, children}) {
     return (
      <div className={styles.container}>
         <div className={styles.div}>
-          <h1 className={styles.title}>Meu primeiro React App</h1>
-         <p className={styles.Text}>
-            Tu sais c'qu'on dit
-            Sois près d'tes amis les plus chers
-            Mais aussi
-            Encore plus près d'tes adversaires
-            Mais ma meilleure ennemie c'est toi
-            Fuis-moi, le pire c'est toi et moi
-            Mais si tu cherches encore ma voix
-            Oublie-moi, le pire c'est toi et moi
+          <h1 className={styles.title}>{title}</h1>
+         <p className={styles.Text}>{children}
+            
          </p>
-        </div>
+
+    </div>
         </div>
     )
 }
