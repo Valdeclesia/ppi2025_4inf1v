8b84c1a26027fce5d45fb3617c7631f663dce088
Valdeclesia <smel73715@gmail.com>
2025-07-23 19:40:00 +0000

    atividade 5 ppi

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,22 @@ import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
 
 export default function App() {
-  
   const [cart, setCart] = useState([]);
-
   function addToCart(product) {
     setCart((prevCart) => [...prevCart, product]);
   }
 
   return (
-    //React Fragment
     <>
+      {}
       <Header cart={cart} />
+
+      {}
       <Routes>
         <Route path="/" element={<ProductList addToCart={addToCart} />} />
-        <Route path="/cart" element={<Cart cart={cart} />} />
+        
+        {}
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
       </Routes>
     </>
   );
